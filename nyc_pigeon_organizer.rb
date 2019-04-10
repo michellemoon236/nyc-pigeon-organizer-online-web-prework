c5d37646ea6d8da4ac269f8918ac04507f103d7c
Michelle M <michellemoon236@gmail.com>
2019-04-10 23:03:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_pigeon_hash = {}
+  #binding.pry
+  data.each do |pigeon_attribute, attribute_data|
+    #binding.pry
+    attribute_data.each do |attribute_type, attribute_value|
+      #binding.pry
+      attribute_value.each do |attribute|
+        #binding.pry
+        attribute_type_array = []
+        attribute_type_array << (attribute_type.to_s)
+        #binding.pry
+        new_pigeon_hash[attribute] = {pigeon_attribute => attribute_type_array}
+        binding.pry
+      end
+    end
+  end
 end
