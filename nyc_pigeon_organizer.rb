65c4455df3f08171c00616ec29d70d121775bb11
Michelle M <michellemoon236@gmail.com>
2019-04-11 00:10:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,6 +9,7 @@ def nyc_pigeon_organizer(data)
       #binding.pry
       attribute_values.each do |pigeon_name|
         #binding.pry
+        if new_pigeon_hash.empty? 
         new_pigeon_hash[pigeon_name] = {}
         new_pigeon_hash[pigeon_name][pigeon_attribute] = []
         new_pigeon_hash[pigeon_name][pigeon_attribute] << attribute_type.to_s
